0a1cbc1d962f81292e24b30aff093269a3e7d084
Tomasz Los <tloszabno@gmail.com>
2019-12-29 23:02:58 +0100

    Add error handling for airly sensor

--- app/sensors/AirlySensor.py
+++ app/sensors/AirlySensor.py
@@ -13,34 +13,56 @@ class AirlySensor(Sensor):
         self.installation_id = installation_id
 
     def measure(self) -> List[Measurement]:
-        headers = {'apikey': self.token, 'Accept': 'application/json'}
-        response = requests.get(
-            'https://airapi.airly.eu/v2/measurements/installation?installationId=' + self.installation_id,
-            headers=headers).json()
-        current = response['current']['values']
-        pm25 = get_value(current, 'PM25')
-        pm10 = get_value(current, 'PM10')
-        temp = get_value(current, 'TEMPERATURE')
-        humidity = get_value(current, 'HUMIDITY')
-        measurements = [
-            Measurement(sensor_name=self.senor_name,
-                        measurement_name="temperature",
-                        unit="C",
-                        value=temp),
-            Measurement(sensor_name=self.senor_name,
-                        measurement_name="humidity",
-                        unit="%",
-                        value=humidity),
-            Measurement(sensor_name=self.senor_name,
-                        measurement_name="pm_10",
-                        unit="ug/m^3",
-                        value=pm10),
-            Measurement(sensor_name=self.senor_name,
-                        measurement_name="pm_2.5",
-                        unit="ug/m^3",
-                        value=pm25),
-        ]
-        return measurements
+        try:
+            headers = {'apikey': self.token, 'Accept': 'application/json'}
+            response = requests.get(
+                'https://airapi.airly.eu/v2/measurements/installation?installationId=' + self.installation_id,
+                headers=headers).json()
+            current = response['current']['values']
+            pm25 = get_value(current, 'PM25')
+            pm10 = get_value(current, 'PM10')
+            temp = get_value(current, 'TEMPERATURE')
+            humidity = get_value(current, 'HUMIDITY')
+            measurements = [
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="temperature",
+                            unit="C",
+                            value=temp),
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="humidity",
+                            unit="%",
+                            value=humidity),
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="pm_10",
+                            unit="ug/m^3",
+                            value=pm10),
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="pm_2.5",
+                            unit="ug/m^3",
+                            value=pm25),
+            ]
+            return measurements
+        except Exception as e:
+            error = str(e)
+            measurements = [
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="temperature",
+                            unit="C",
+                            error=error),
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="humidity",
+                            unit="%",
+                            error=error),
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="pm_10",
+                            unit="ug/m^3",
+                            error=error),
+                Measurement(sensor_name=self.senor_name,
+                            measurement_name="pm_2.5",
+                            unit="ug/m^3",
+                            error=error),
+            ]
+            return measurements
 
 
 def get_value(value_list, name):
