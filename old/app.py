292b03829e209eec4d0c69d334f1291794a6643a
Tomasz Los <tloszabno@gmail.com>
2019-12-30 13:20:42 +0100

    Remove old implementation

--- old/app.py
+++
@@ -1,7 +0,0 @@
-import sys
-from loremote_sensors.main import main
-
-
-
-if __name__ == '__main__':
-    main(mocked=(len(sys.argv) > 1 and sys.argv[1] == "--mocked"))
--- old/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DESTINATION="pi@192.168.1.3:/home/pi/apps"
-
-rsync -avzH  --partial /home/tomek/workspace/my/loremote_sensors $DESTINATION/
