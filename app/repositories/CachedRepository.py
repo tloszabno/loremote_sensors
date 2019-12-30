c5019732bdcb719fe04fb6328084f613c8956ef5
Tomasz Los <tloszabno@gmail.com>
2019-12-30 13:19:56 +0100

    Add cache shink

--- app/repositories/CachedRepository.py
+++ app/repositories/CachedRepository.py
@@ -7,9 +7,12 @@ from app.repositories.Repository import Repository
 class CachedRepository(Repository):
     def __init__(self, elements):
         self.cache = elements[:]
+        self.init_elements_size = len(elements)
 
     def save(self, measurement: MeasurementsSet):
         self.cache.append(measurement)
+        if len(self.cache) * 1.5 > self.init_elements_size:
+            self.cache = self.cache[-self.init_elements_size:]
 
     def get_last(self, max=100) -> List[MeasurementsSet]:
         number_of_elements = len(self.cache)
