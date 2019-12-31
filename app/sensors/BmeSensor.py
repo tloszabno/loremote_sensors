db32f78d95517e98d5e088c5b801e7f3af43dde7
Tomasz Los <tloszabno@gmail.com>
2019-12-31 13:40:25 +0100

    Fix unit for pressure

--- app/sensors/BmeSensor.py
+++ app/sensors/BmeSensor.py
@@ -9,14 +9,24 @@ class BmeSensor(Sensor):
         self.sensor_name = sensor_name
 
     def measure(self) -> List[Measurement]:
-        from app.sensors.thirdparty.bme280 import Bme280, MODE_FORCED
-        sensor = Bme280()
-        sensor.set_mode(MODE_FORCED)
-        temp_data, pressure_data, humidity_data = sensor.get_data()
+        error, humidity_data, pressure_data, temp_data = self.__measure__()
         temperature = Measurement(sensor_name=self.sensor_name, measurement_name="temperature", value=temp_data,
-                                  unit="C")
+                                  unit="C", error=error)
         humidity = Measurement(sensor_name=self.sensor_name, measurement_name="humidity", value=float(humidity_data),
-                               unit="%")
+                               unit="%", error=error)
         pressure = Measurement(sensor_name=self.sensor_name, measurement_name="pressure", value=float(pressure_data),
-                               unit="%")
+                               unit="hPa", error=error)
         return [temperature, humidity, pressure]
+
+    @staticmethod
+    def __measure__():
+        temp_data, pressure_data, humidity_data = 0.0, 0.0, 0.0
+        error = ""
+        try:
+            from app.sensors.thirdparty.bme280 import Bme280, MODE_FORCED
+            sensor = Bme280()
+            sensor.set_mode(MODE_FORCED)
+            temp_data, pressure_data, humidity_data = sensor.get_data()
+        except Exception as e:
+            error = str(e)
+        return error, humidity_data, pressure_data / 100.0, temp_data
