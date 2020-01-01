ab46bededfa1c50221759a2af5484440a8b03869
Tomasz Los <tloszabno@gmail.com>
2020-01-01 19:36:36 +0100

    Fix for retries in humid sensor

--- app/sensors/HumidSensor.py
+++ app/sensors/HumidSensor.py
@@ -1,9 +1,11 @@
 import logging
+import time
 
 from app.measurement.Measurement import Measurement, MeasurementUnits, MeasurementTypes
 from app.sensors.Sensor import Sensor
 
-NUMBER_OF_READS = 3
+NUMBER_OF_READS = 4
+INTERVAL_BETWEEN_MEASURES_IN_S = 10
 RETRIES = 3
 
 logger = logging.getLogger('HumidSensor')
@@ -17,7 +19,8 @@ class HumidSensor(Sensor):
     def measure(self):
         temp_value, humidity_value, error = 0.0, 0.0, ""
         try:
-            readings = [self.__get_readings_with_retry__(self.port) for _ in range(NUMBER_OF_READS)]
+            readings = [self.__get_readings_with_retry__(self.port, i) for i in range(NUMBER_OF_READS)]
+            readings = filter(lambda x:  x is not None and x[0] is not None and x[1] is not None, readings)
             avgs = tuple(map(lambda y: sum(y) / float(len(y)), zip(*readings)))
             temp_value = float(avgs[1])
             humidity_value = float(avgs[0])
@@ -32,8 +35,10 @@ class HumidSensor(Sensor):
                                unit=MeasurementUnits.HUMIDITY, error=error)
         return [temperature, humidity]
 
-    def __get_readings_with_retry__(self, port, attempt=0):
+    def __get_readings_with_retry__(self, port, i, attempt=0):
         try:
+            if i > 0:
+                time.sleep(INTERVAL_BETWEEN_MEASURES_IN_S)
             import Adafruit_DHT
             read = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, port)
             logger.info("Humid reading: %s" % str(read))
@@ -42,4 +47,4 @@ class HumidSensor(Sensor):
             if attempt >= RETRIES:
                 raise e
             else:
-                return self.__get_readings_with_retry__(port=port, attempt=attempt + 1)
+                return self.__get_readings_with_retry__(port=port, i=i, attempt=attempt + 1)
