ddb86e257504eef9934ef41c8ed078908224fc93
Tomasz Los <tloszabno@gmail.com>
2020-01-01 19:41:41 +0100

    Add gathering sensor results in separate threads

--- app/measurement/MeasurementService.py
+++ app/measurement/MeasurementService.py
@@ -4,6 +4,7 @@ from app.listeners.Listener import Listener
 from app.repositories.Repository import Repository
 from app.sensors.Sensor import Sensor
 from app.measurement.Measurement import MeasurementsSet
+from concurrent.futures import ThreadPoolExecutor
 
 
 class MeasurementService(object):
@@ -11,11 +12,13 @@ class MeasurementService(object):
         self.sensors = sensors
         self.listeners = listeners
         self.repository = repository
+        self.backgroundExecutor = ThreadPoolExecutor()
 
     def measure(self):
-        all_measurements = []
+        all_measurements_futures = []
         for sensor in self.sensors:
-            all_measurements += sensor.measure()
+            all_measurements_futures += self.backgroundExecutor.submit(sensor.measure)
+        all_measurements = [x.result() for x in all_measurements_futures]
         measurement = MeasurementsSet(all_measurements)
         self.repository.save(measurement)
         for repository in self.listeners:
